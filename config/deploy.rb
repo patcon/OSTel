c8a257a3b06d66f7c5fadea5ca41cb104abff0d0
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-10-20 01:50:27 -0300

    Configure auto restart of nginx and unicorn on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,6 +48,8 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute :sudo, :service, :nginx, :reload
+      execute :sudo, :service, :unicorn_ostn, :restart
     end
   end
 
