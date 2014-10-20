d0994c7ef5f981220fa9c9b47a6944dec9789eb6
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-10-20 03:44:58 -0300

    Add linked_dirs for unicorn.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, "/home/deploy/#{app}"
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
