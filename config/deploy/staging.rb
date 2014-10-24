da37e2c31d37f2d71593b9e5c8d6beac17f4a1cd
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-10-24 06:10:57 -0300

    Remove hardcoded server ip in favour of ostn-test hostname.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@162.243.200.49}
-role :web, %w{deploy@162.243.200.49}
-role :db,  %w{deploy@162.243.200.49}
+role :app, %w{deploy@ostn-test}
+role :web, %w{deploy@ostn-test}
+role :db,  %w{deploy@ostn-test}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@162.243.200.49}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-#server '162.243.200.49', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
